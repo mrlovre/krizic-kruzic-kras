e9e2a215b7347eca921f4cfe1d1f3a357af35d3e
Lovre Mrčela <lovre.mrcela@fer.hr>
2016-09-18 08:55:39 +0200

    Playar.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+var currentPlayar;
+var state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -10,6 +14,21 @@ app.use('/css', express.static(__dirname + '/css'));
 
 app.use('/js', express.static(__dirname + '/js'));
 
-http.listen(3000, function() {
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
+
+io.on('connection', function (socket) {
+  socket.on('play', function (msg) {
+    if (currentPlayar !== msg.playar && !state[msg.field - 1]) {
+      state[msg.field - 1] = msg.playar;
+      io.emit('play-confirmed', msg);
+      console.log(msg.playar + ' plays ' + msg.field);
+      currentPlayar = msg.playar;
+    }
+  });
+});
+
+var checkGameOver = function() {
+  
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-enterName = function() {
+var socket;
+
+var init = function () {
+    socket = io();
+    $('#game button').each(function () {
+        var c = $(this).attr('id')[1];
+        $(this).on('click', function () {
+            socket.emit('play', { field: c, playar: playar });
+        });
+    });
+    socket.on('play-confirmed', function (msg) {
+        var b = $('#f' + msg.field);
+        if (msg.playar === window.playar) {
+            b.addClass('btn-success');
+        } else {
+            b.addClass('btn-danger');
+        }
+    });
+}
+
+var enterName = function () {
+    var playar = $('#playar-input').val();
+    if (!playar) {
+        return false;
+    }
+    window.playar = playar;
     $('#playar').hide();
     $('#game').fadeTo('slow', 1);
+}
+
+var playarChange = function () {
+    var playar = $('#playar-input').val();
+    if (!playar) {
+        $('#btn-gaw').addClass('disabled')
+    } else {
+        $('#btn-gaw').removeClass('disabled');
+    }
 }
